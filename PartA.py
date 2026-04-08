76e15504102bb9103b6b72e5b82c62b73e28f966
Anna Kuskova <anna.l.kuskova@gmail.com>
2026-04-08 12:00:22 -0700

    added comments, small changes ie non-english handle

--- PartA.py
+++ PartA.py
@@ -2,26 +2,30 @@ import sys
 def main():
     pass
 
-def tokenize(textFilePath): # returns a list of tokens
+'''
+O(n) - linear to the size of the file, since isalnum, isascii, append and lower are constant
+'''
+def tokenize(textFilePath):
     tokens = []
     newToken = ""
     with open(textFilePath, 'r') as file:
         for line in file:
-            line = line.casefold()
             for char in line:
-                if char.isalnum():
+                if char.isalnum() and char.isascii():
                     newToken += char
                 else:
-                    if newToken != "":
-                        tokens.append(newToken)
+                    if newToken != "" :
+                        tokens.append(newToken.lower())
                         newToken = ""
 
     if newToken != "":
         tokens.append(newToken)
 
     return tokens
-
-def computeWordFrequencies(listOfTokens): # returns a hash map of frequencies
+'''
+O(n) - linear to the number of the tokens, since hash operations are constant (on average)
+'''
+def computeWordFrequencies(listOfTokens):
     tokenMap = {}
     for item in listOfTokens:
         if item not in tokenMap:
@@ -30,14 +34,20 @@ def computeWordFrequencies(listOfTokens): # returns a hash map of frequencies
             tokenMap[item] = tokenMap.get(item) + 1
     return tokenMap
 
-
-def print(frequencies): # prints frequencies
+'''
+O(nlogn) - since sorted() runs in O(nlogn) and for loop is iterating over dictionary in O(n), O(nlogn) dominates
+'''
+def print(frequencies):
+    frequencies = dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
     for x,y in frequencies.items():
         sys.stdout.write(f"{x} => {y} \n")
 
 
 if __name__ == "__main__":
-    main()
+    textFilePath = sys.argv[1]
+    tokens = tokenize(textFilePath)
+    frequencies = computeWordFrequencies(tokens)
+    print(frequencies)
 
 
 
--- PartB.py
+++ PartB.py
@@ -1,9 +1,12 @@
 from PartA import tokenize 
 from PartA import computeWordFrequencies
+import sys
 
 def main():
     pass
-
+'''
+O(n) - linear to the sizes of the files
+'''
 def intersection(file1, file2):
     list1, list2 = tokenize(file1), tokenize(file2)
     map1, map2 = computeWordFrequencies(list1), computeWordFrequencies(list2)
@@ -12,3 +15,5 @@ def intersection(file1, file2):
 
 if __name__ == "__main__":
     main()
+    file1, file2 = sys.argv[1:3]
+    intersection(file1, file2)
